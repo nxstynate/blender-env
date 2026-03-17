a01429627a7731ad8bd369f848415f6303e28545
Nate <nathanael.paul.lewis@gmail.com>
2026-03-17 08:27:26 -0500

    Updated files on 2026-03-17

--- install/Blender.ps1
+++ install/Blender.ps1
@@ -1,7 +1,7 @@
 # Installs Blender and sets up work environment.
 
 $ver = "4.5"
-$verSub = ".6"
+$verSub = ".7"
 $blenderPath = "$HOME/programs"
 $extension = ".zip"
 $package = "blender-$ver$verSub-windows-x64"
